e667ecb68ed0a7b2faca15021ff96e23db59171e
zjh <junhuizoujh@163.com>
2024-06-06 10:59:21 +0800

    001

--- 01-Funddamentals-part-1/script.js
+++ 01-Funddamentals-part-1/script.js
@@ -1,5 +1,27 @@
+/*
 let a = "ama";
-if (a === "ama") {
-  alert("aaaaa");
-}
 console.log(123);
+console.log("john");
+
+let jsIsFun = true;
+console.log(jsIsFun);
+console.log(typeof true);
+console.log(typeof 23);
+
+let year;
+console.log(year);
+console.log(typeof year);
+year = 1999;
+console.log(year);
+*/
+
+/*
+let age = 30;
+age = 31;
+const b = 1111;
+//b = 222;常量不可改变
+*/
+
+const a = "a";
+const b = "b";
+console.log(a + b);
